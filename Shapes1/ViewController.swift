e7730c37166f12597e793ba3e8c66d82f709ee2e
magnus-sg <weesiang@outlook.com>
2015-07-12 10:28:31 +0800

    First commit

---
+++ Shapes1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shapes1
+//
+//  Created by Gay Wee Siang on 11/7/15.
+//  Copyright (c) 2015 Magnus International, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
